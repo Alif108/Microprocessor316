7167111147aa61a951cbcecdc08421a3ac6e0cfd
Alif108 <46017890+Alif108@users.noreply.github.com>
2021-06-05 13:43:21 +0600

    offline 6

--- Offline_6/main.c
+++ Offline_6/main.c
@@ -1,5 +1,5 @@
 #ifndef F_CPU
-#define F_CPU 16000000UL // 16 MHz clock speed
+#define F_CPU 1000000UL // 1 MHz clock speed
 #endif
 
 #define D4 eS_PORTD4
@@ -16,33 +16,8 @@
 #include "lcd.h"
 #include <avr/io.h>
 
-/*void float_to_string(char x[], float y)
-{
-	
-	int z = y * 100;
-	char temp[5] = "00000";
-	sprintf(temp, "%d", z);
-	
-	if(y>1)
-	{
-		x[0] = temp[0];
-		x[1] = '.';
-		x[2] = temp[1];
-		x[3] = temp[2];
-	}
-	else
-	{
-		x[0] = '0';
-		x[1] = '.';
-		x[2] = temp[0];
-		x[3] = temp[1];
-	}
-}*/
-
 int main(void)
 {
-	unsigned int result_upper = 0;
-	unsigned int result_lower = 0;
 	unsigned int result = 0;
 	float analogue_value = 0.0;
 	
@@ -52,8 +27,8 @@ int main(void)
 	DDRD = 0xFF;
 	DDRC = 0xFF;
 	
-	ADMUX = 0x62;			// 0110 0010        // REF -> 01 ;		ADLAR -> 1 ;		MUX -> 00010	
-	ADCSRA = 0x83;			// 1000 0011		// ADEN, ADSC, ADATE, ADIF, ADIE -> 10000;		ADPS2, ADPS1, ADPS0 -> 011		
+	ADMUX = 0x62;									// 0110 0010        // REF -> 01 ;		ADLAR -> 1 ;		MUX -> 00010	
+	ADCSRA = 0x83;									// 1000 0011		// ADEN, ADSC, ADATE, ADIF, ADIE -> 10000;		ADPS2, ADPS1, ADPS0 -> 011		
 	
 	Lcd4_Init();
 	
@@ -63,12 +38,8 @@ int main(void)
 			
 		while(ADCSRA & (1 << ADSC));				// wait till conversion ends
 		
-		result_lower = ADCL>>8;						// reading ADCL first
-		result_upper = ADCH<<2;
-		result = result_upper | result_lower;	
+		result = (ADCL>>6) | (ADCH<<2);				// reading ADCL first
 		analogue_value = (float) result * 5/1024;	// getting the actual value
-		
-		//Lcd4_Clear();
 		
 		Lcd4_Set_Cursor(1,0);
 		sprintf(message, "Voltage: ");
@@ -77,7 +48,5 @@ int main(void)
 		Lcd4_Set_Cursor(1,9);
 		dtostrf(analogue_value, 4, 2, float_value);
 		Lcd4_Write_String(float_value);
-
-		//_delay_ms(500);
 	}
 }
